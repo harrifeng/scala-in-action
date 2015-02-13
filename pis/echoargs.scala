7981d15443dfa0aeed1958f02e690c7a41bf09dc
harri feng <harrifeng@gmail.com>
2015-02-13 15:25:29 +0800

    programming in scala added

---
+++ pis/echoargs.scala
@@ -0,0 +1,15 @@
+var i = 0
+while (i < args.length) {
+  if (i != 0) {
+    print(" ")
+  }
+  print(args(i))
+  i += 1
+}
+println()
+
+////////////////////////////////////////////////////
+// <===================OUTPUT===================> //
+// > scala echoargs.scala Scala is even more fun  //
+// Scala is even more fun                         //
+////////////////////////////////////////////////////
